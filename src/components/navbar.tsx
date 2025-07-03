42f585746bcc870b0173730b4468b8353a0b81aa
himanshi <hggupta.2004@gmail.com>
2025-07-03 13:52:56 +0530

    Add all project files

---
+++ src/components/navbar.tsx
@@ -0,0 +1,74 @@
+'use client'
+import Link from "next/link"
+import { signIn } from 'next-auth/react';
+import { useSession,signOut } from "next-auth/react"
+import { User } from "next-auth"
+import { Button } from "./ui/button"
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
+
+
+
+
+function Navbar() {
+    const router = useRouter()
+
+    const { toast } = useToast()
+
+    const {data : session} = useSession()
+
+    const user :User = session?.user
+
+    const demo = async ()=>{
+        const res = await signIn('credentials',{
+            redirect: false,
+            identifier : "demo@gmail.com",
+            password : "12345678"
+          })
+    
+          if(res?.error){
+            if(res.error == 'CredentialsSignin'){
+              toast({
+                title : 'Login Failed',
+                description : 'Incorect username or passeord',
+                variant : 'destructive'
+              })
+            }
+            else{
+              toast({
+                description: res.error,
+                variant: 'destructive',
+            })
+            }
+          }
+          if (res?.url) {
+            router.replace('/dashboard');
+          }
+    }
+
+  return (
+    <nav className="p-4 md:p-6 bg-slate-200 shadow-lg">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <a href="#" className="text-4xl font-bold mb-4 md:mb-0">SilentWhispers</a>
+        {
+            session?(
+                <>
+                    <span className="mr-4 text-lg font-semibold">Welcome, {user.username || user.email}</span>
+                    <Button className="w-full md:w-auto" onClick={()=>signOut()} variant="destructive">SignOut</Button>
+
+                </>
+            ):(
+                <div className="space-x-3">
+                    <Link href='/sign-in'>
+                        <Button className="w-full md:w-auto">Login</Button>
+                    </Link>
+                    <Button className="w-full md:w-auto" onClick={demo} variant="destructive">Trial</Button>
+                </div>
+            )
+        }
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,31 @@
+import MailHelper from "@/helpers/nodeMailer"
+import EmailTemplate from "@/helpers/EmailTemplate"
+
+
+export async function sendVerificationMail(
+    email: string,
+    username: string,
+    verificationCode: string
+): Promise<any> {
+
+    try {
+        const html = `  <h3>Hello User <b>${username}</b> ,Congrats on registering on SilentWhispers.</h3>
+        <p>Your verification Code is</p> <h1>${verificationCode}</h1>
+        `
+
+        // console.log(process.env.EMAIL)
+        // console.log(process.env.PASSWORD)
+        const res = await MailHelper({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Silent  Whispers verification code',
+            html: EmailTemplate({ username, code: verificationCode }),
+            text: `Your verification code is ${verificationCode}`
+        });
+        console.log(res)
+        return { success: true, message: "Verification code sent successfully" }
+    } catch (error) {
+        return { success: false, message: "Error in sending verification code" }
+    }
+}
+
